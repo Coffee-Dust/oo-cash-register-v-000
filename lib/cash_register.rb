ef322080fc3fe72ec785df607bd06de9f1a5f256
Jonathan Dust <d24dust@hotmail.com>
2018-04-18 21:34:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  attr_reader :total, :items, :discount
 
+  def initialize(discount=nil)
+    @total = 0
+    @items = []
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += price * quantity
+    @items << title
+  end
+
+  def apply_discount
+    if @discount != nil
+      
+    else
+      return "No discount to apply"
+    end
+  end
+
+end
